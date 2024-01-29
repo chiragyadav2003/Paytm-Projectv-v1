95b723fba7e56de2fccb5a23a1cc4288fd54c80b
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-01-29 16:47:52 +0530

    managing routing for express

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,19 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const rootRouter = require("./routes/index")
+
+const app = express();
+const PORT = 8080;
+
+//using middleware
+app.use(express.json())
+
+//using routing
+app.use('/api/v1', rootRouter)
+
+
+
+app.listen(PORT, (err) => {
+    if (err) console.log("error in server listening", err)
+    console.log(`app is running on port - ${PORT}`)
+})
+
---
+++ backend/routes/index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const userRouter = require("./user")
+
+const rootRouter = express.Router()
+
+//user request routed to user router
+router.use("/user", userRouter)
+
+module.exports = rootRouter
+
+//rootRouter will handle request like this
+// api/v1/user
+// api/v1/transaction
+
+
+// app.use('/api/v1/', rootRouter) tells Express to use the rootRouter middleware function to handle any incoming HTTP requests that match the /api/v1/ route path prefix. This means that any requests to URLs starting with /api/v1/ will be routed to the rootRouter for further processing, where the appropriate route handlers defined in rootRouter will handle them.
