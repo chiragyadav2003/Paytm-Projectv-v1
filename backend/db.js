47c62b951324a405e0e919efece880711907f897
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-01-29 15:32:45 +0530

    creating user model and connection to db

---
+++ backend/db.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose")
+require('dotenv').config()
+
+// console.log(process.env.MONGODB_URL + process.env.DB_NAME)
+
+//connect to mongoose
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGODB_URL + process.env.DB_NAME);
+        console.log(`Mongo db connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.log("mongodb connection error : Error = ", error)
+        process.exit(1);
+    }
+}
+// connectDB()
+
+const UserSchema = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            required: true,
+        },
+        lastName: {
+            type: String,
+            required: true,
+        },
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            lowercase: true,
+            minLength: 3,
+            maxLength: 30
+        },
+        password: {
+            type: String,
+            required: true,
+            minLength: 6
+        }
+    },
+    {
+        timestamps: true
+    })
+
+const User = mongoose.model("User", UserSchema)
+
+module.exports = {
+    User,
+    connectDB
+}
