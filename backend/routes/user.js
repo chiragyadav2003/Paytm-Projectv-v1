00bdabf58b4cfb9de36731d706fb25d374763cd0
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-01-29 23:06:05 +0530

    adding signin route with authentication

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -6,29 +6,29 @@ const jwt = require("jsonwebtoken")
 
 const userRouter = express.Router()
 
-const userSchema = zod.object({
-    username: zod.string().email(),
-    firstName: zod.string(),
-    lastName: zod.string(),
-    password: zod.string().min(6)
-})
-
 userRouter.get("/hi", (req, res) => {
     res.json({
         msg: "hi, hope you are doing well"
     })
 })
 
+const signupBody = zod.object({
+    username: zod.string().email(),
+    firstName: zod.string(),
+    lastName: zod.string(),
+    password: zod.string().min(6)
+})
+
 userRouter.post('/signup', async (req, res) => {
     const userData = req.body;
 
     //check for user i/p validation using zod
-    const zodUserSchema = userSchema.safeParse(userData)
+    const signupZodStatus = signupBody.safeParse(userData)
     // console.log(zodUserSchema)
 
     //for invalid input return status code - 411
-    if (zodUserSchema.success == false) {
-        console.log("--\nzod validation is false - ", zodUserSchema.error)
+    if (signupZodStatus.success == false) {
+        console.log("--\nzod validation is false - ", signupZodStatus.error)
         return res.status(411).json({
             msg: "zod validation failed"
         })
@@ -85,6 +85,65 @@ userRouter.post('/signup', async (req, res) => {
             msg: "user creation failed"
         })
     }
+})
+
+const signinBody = zod.object({
+    username: zod.string().email(),
+    password: zod.string().min(6)
+})
+userRouter.post('/signin', async (req, res) => {
+
+    const { username, password } = req.body;
+    if (!username || !password) {
+        return res.status(411).json({
+            message: "Username or Password not present",
+        })
+    }
+
+    //zod validation
+    const { success } = signinBody.safeParse(req.body)
+    //zod validation failed
+    if (!success) {
+        console.log("--\nzod validation is false - ", signupZodStatus.error)
+        return res.status(411).json({
+            msg: "zod validation failed"
+        })
+    }
+
+    //make db request to check if there is any entry corrospond to given username and password
+    try {
+        const user = await User.findOne({ username, password })
+        if (!user) {
+            console.log("signin failed as there is no user corrospond to given username and password")
+            res.status(411).json({
+                message: "Error while logging in"
+            })
+        }
+        else {
+            let token
+            try {
+                //jwt generation for user credential username, secretkey which will expire in 1 hour
+                token = jwt.sign({ username: username, }, jwtSecret, { expiresIn: "1h" })
+                console.log(token)
+                res.status(200).json({
+                    message: "login successful",
+                    token: token
+                })
+            } catch (err) {
+                console.log(err);
+                console.log("jwt generation failed")
+                const error = new Error("Error! Something went wrong.");
+                return next(error);
+            }
+        }
+    } catch (error) {
+        console.log("request to db for authentication failed-error encountered")
+        res.status(411).json({
+            msg: "request to db for authentication failed-error encountered",
+            error: error.message,
+        })
+    }
+
 })
 
 
