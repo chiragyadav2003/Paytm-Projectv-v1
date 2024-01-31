c41e01aaf2c659af9551e4ba8ebd559c2216dd1f
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-02-01 03:44:46 +0530

    Adding signin functionality

--- Frontend/src/pages/Signin.jsx
+++ Frontend/src/pages/Signin.jsx
@@ -1,16 +1,42 @@
+import { useState } from "react"
 import { Heading, SubHeading, InputBox, Button, BottomWarning } from "../components/index"
+import { useNavigate } from "react-router-dom"
+import axios from "axios"
 
 function SignIn() {
+
+    const [userName, setUserName] = useState("")
+    const [password, setPassword] = useState("")
+
+    const navigate = useNavigate()
+
     return (
         <div className="bg-slate-300 h-screen flex justify-center">
             <div className="flex flex-col justify-center">
                 <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
                     <Heading label={"Sign in"} />
                     <SubHeading label={"Enter your  credentials to access your account"} />
-                    <InputBox placeholder={"johnCena@gmail.com"} label={"Email"} />
-                    <InputBox placeholder={"123212"} label={"Password"} />
+                    <InputBox onChange={(e) => setUserName(e.target.value)} placeholder={"johnCena@gmail.com"} label={"Email"} />
+                    <InputBox onChange={(e) => setPassword(e.target.value)} placeholder={"123212"} label={"Password"} />
                     <div className="pt-4">
-                        <Button label={"Sign in"} />
+                        <Button onClick={async () => {
+                            try {
+                                const res = await axios.post(`http://localhost:3000/api/v1/user/signin`, {
+                                    userName,
+                                    password
+                                })
+                                const status = res.data.success
+                                console.log(status)
+                                if (status) {
+                                    localStorage.setItem("token", res.data.token)
+                                    navigate("/dashboard")
+                                }
+                            } catch (error) {
+                                alert("invalid user")
+                                console.log("login error")
+                            }
+
+                        }} label={"Sign in"} />
                     </div>
                     <BottomWarning label={"Don't have an account?"} buttonText={"Sign up"} to={"/signup"} />
                 </div>
