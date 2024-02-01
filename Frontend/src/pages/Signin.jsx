f5d9b37b383b706791884a11b3aa3478dd3c09a2
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-02-01 14:37:09 +0530

    styling

--- Frontend/src/pages/Signin.jsx
+++ Frontend/src/pages/Signin.jsx
@@ -11,7 +11,7 @@ function SignIn() {
     const navigate = useNavigate()
 
     return (
-        <div className="bg-slate-300 h-screen flex justify-center">
+        <div className="bg-slate-600  h-screen flex justify-center">
             <div className="flex flex-col justify-center">
                 <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
                     <Heading label={"Sign in"} />
--- Frontend/src/pages/Signup.jsx
+++ Frontend/src/pages/Signup.jsx
@@ -13,7 +13,7 @@ function SignUp() {
 
 
     return (
-        <div className="bg-slate-300 h-screen flex justify-center">
+        <div className="bg-slate-600 h-screen flex justify-center">
             <div className="flex flex-col justify-center">
                 <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
                     <Heading label={"Sign up"} />
